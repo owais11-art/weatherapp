9ac2e8ceb861401e313ed4378a2e69230b5982d2
owais_zahoor <owaiszahoor79@gmail.com>
2022-01-20 14:03:31 +0530

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,136 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="weather">
+    <h1 class="head">WEATHER</h1>
+    <Form @onSubmit="handleSubmit" />
+    <div class="temp">
+      <h1> {{temp}}°С </h1>
+      <p>{{status}}</p>
+      <p class="place">{{name}}</p>
+      <div class="temps">
+        <div class="max-temp">
+          <h5>Humidity</h5>
+          <p>{{humidity}}</p>
+        </div>
+        <div class="min-temp">
+          <h5>Pressure</h5>
+          <p>{{pressure}}</p>
+        </div>
+      </div>
+      <div class="icon">
+        <img :src="icn" alt="">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from '@vue/reactivity';
+import Form from './components/Form.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Form
+  },
+  setup(){
+    let temp = ref('');
+    let humidity = ref('');
+    let pressure = ref('');
+    let name = ref('');
+    let icn = ref('');
+    let status = ref('');
+    const getData = (place = "Srinagar") => {
+      fetch(`http://api.weatherapi.com/v1/current.json?key=b7993fbeccba47158bf164910221901&q=${place}&aqi=no`)
+      .then(res => res.json())
+      .then(data => {
+        temp.value = data.current.temp_c;
+        status.value = data.current.condition.text;
+        icn.value = data.current.condition.icon;
+        name.value = data.location.name;
+        humidity.value = data.current.humidity;
+        pressure.value= data.current.pressure_in;
+      })
+    };
+    const handleSubmit = place => getData(place);
+    getData();
+      return {
+        temp,
+        status,
+        icn,
+        name,
+        humidity,
+        pressure,
+        handleSubmit
+      };
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  background: #ccc;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+.weather{
+  width: 300px;
+  border-radius: 10px;
+  background: #fff;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 10px 5px 10px rgba(0, 0, 0, 0.5);
+}
+.weather .temp{
+  position: relative;
+  width: 150px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  margin: 30px auto;
+  text-align: center;
+}
+.weather .temp .place{
+  color: orangered;
+  font-weight: bold;
+}
+.weather .temp p{
+  margin-top: 8px;
+}
+.weather .temp .temps{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 5px;
+  margin-top: 20px;
+}
+.weather .temp .temps div h5{
+  color: rgb(25, 25, 167);
+}
+.weather .temp .temps div p{
+  margin-top: 8px;
+}
+.weather .temp .icon{
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  background: #ccc;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+}
+.weather .temp .icon img{
+  width: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,58 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <div class="form-control control-input">
+          <input type="text" placeholder="Enter place" v-model="area">
+      </div>
+      <div class="form-control control-search">
+          <input type="submit" value="Search">
+      </div>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    name: 'Form',
+    emits: ['onSubmit'],
+    setup(props, { emit }){
+        let area = ref('');
+        const handleSubmit = () => {
+            emit('onSubmit', area.value);
+            area.value = '';
+        };
+        return { area, handleSubmit };
+    }
+
+}
+</script>
+
+<style>
+    form{
+        display: grid;
+        gap: 10px;
+    }
+    form .form-control{
+        width: 100%;
+    }
+    form .control-input{
+        border: 1px solid #ccc;
+    }
+    form input{
+        width: 100%;
+        border: none;
+        padding: 8px;
+    }
+    form input[type="text"]:focus{
+        outline: 1px solid rgb(18, 196, 18);
+    }
+    form input[type="submit"]{
+        background: rgb(18, 196, 18);
+        color: #ccc;
+        font-weight: bold;
+        letter-spacing: 1px;
+        cursor: pointer;
+    }
+    form input[type="submit"]:hover{
+        background: rgb(6, 175, 6);
+    }
+</style>
